4b55943ab30f2a61e9cae9281142287d7c8e1f48
Viktoriia <viktoriia.sarapii@devit-team.com>
2020-12-19 02:14:40 +0000

    change port

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 require('./config/mongoose')
 const app = express()
-const port = 3000
+const port = 6969
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.get('/', (req, res) => res.send('Hello World with Express'));

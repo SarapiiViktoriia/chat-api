4f31959967edea46885be57ef6e2cb9ada138470
Viktoriia <viktoriia.sarapii@devit-team.com>
2021-01-11 20:05:00 +0000

    add upload avatar API

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,3 @@
-const multer = require('multer')
 const User = require('../models/userModel')
 exports.register = async (req, res) => {
   const user = new User(req.body)
@@ -30,6 +29,14 @@ exports.users = async (req, res) => {
     res.status(500).send()
   }
 }
-exports.show = async(req, res)=>{
+exports.show = async (req, res) => {
   res.send(req.user)
 }
+exports.upload = async (req, res) => {
+  req.user.avatar = req.file.buffer
+  await req.user.save()
+  res.send({ message: 'berhasil di upload' })
+},
+  (error, req, res) => {
+    res.status(400).send({ error: error.message })
+  }
--- src/routes/User.js
+++ src/routes/User.js
@@ -1,8 +1,18 @@
 const auth = require('../middleware/auth')
+const multer = require('multer')
+const upload = multer({
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error('Please upload an image'))
+    }
+    cb(undefined, true)
+  },
+})
 module.exports = app => {
   const userController = require('../controllers/userController')
   app.post('/api/user/register', userController.register)
   app.post('/api/user/login', userController.login)
   app.get('/api/user/me', auth, userController.show)
   app.get('/api/users', auth, userController.users)
+  app.post('/api/user/me/avatar', auth,upload.single('avatar'), userController.upload)
 }

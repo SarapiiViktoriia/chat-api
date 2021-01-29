6a3402173a33b64b58859ac9ac262fd60d9cc61c
Viktoriia <viktoriia.sarapii@devit-team.com>
2021-01-29 00:35:17 +0000

    auth in avatar API

--- src/routes/User.js
+++ src/routes/User.js
@@ -1,6 +1,6 @@
 const auth = require('../middleware/auth')
 const multer = require('multer')
-const path = require('path');
+const path = require('path')
 module.exports = app => {
   const userController = require('../controllers/userController')
   app.post('/api/users/register', userController.register)
@@ -8,5 +8,5 @@ module.exports = app => {
   app.get('/api/users/me', auth, userController.show)
   app.put('/api/users/:id', auth, userController.update)
   app.get('/api/users', auth, userController.index)
-  app.post('/api/users/avatar/:id', userController.uploadAvatar)
+  app.post('/api/users/avatar/:id', auth, userController.uploadAvatar)
 }

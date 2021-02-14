8e608e59e98edbefdf5bc329b2f27d1e9336837f
Viktoriia <viktoriia.sarapii@devit-team.com>
2021-02-14 22:04:10 +0000

    cleanUP

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,9 +1,8 @@
 const Message = require('../models/messageModel')
 exports.sendMessage = async (req, res) => {
-  var message = new Message(req.body);
-  message.save((err) => {
-    if (err)
-      sendStatus(500)
+  var message = new Message(req.body)
+  message.save(err => {
+    if (err) sendStatus(500)
     global.io.emit('message', req.body)
     res.sendStatus(200)
   })

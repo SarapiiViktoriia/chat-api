a1bde70cf3ec8ff1ad546c56f2c01d35a29721ec
Viktoriia <viktoriia.sarapii@devit-team.com>
2021-02-17 01:39:32 +0000

    modified socket.js

--- sockets.js
+++ sockets.js
@@ -7,10 +7,6 @@ var io = require('socket.io').listen(server);
 global.io = io; 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-app.use(express.static(__dirname + '/public'))
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/views/index.html')
-})
 const messageRouter = require('./routes/message')(app)
 io.on('connection', () => {
     console.log('a user is connected')

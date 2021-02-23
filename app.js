248b7097681e4a38898e5c3be03caba8c3e6e035
Viktoriia <viktoriia.sarapii@devit-team.com>
2021-02-23 23:03:09 +0000

    add tailwindcss

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const mongoose = require('./config/mongoose')
 const bodyParser = require('body-parser')
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(express.static(__dirname + '/public'))
 const userRouter = require('./routes/user')(app)
 const roomChatRouter = require('./routes/roomChat')(app)
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/public/views/chats.html');
-})
 const port = 6969
 app.listen(port, () => {
   console.log(`Server is running in ${port}`)
--- sockets.js
+++ sockets.js
@@ -1,18 +1,27 @@
 const mongoose = require('./config/mongoose')
 var express = require('express');
 var bodyParser = require('body-parser')
+var cors = require('cors')
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 global.io = io;
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(express.static(__dirname + '/public'))
 const messageRouter = require('./routes/message')(app)
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/public/views/chats.html');
+app.get('/', function (req, res, next) {
+    res.sendFile(__dirname + '/public/views/register.html');
 })
-io.on('connection', () => {
-    console.log('a user is connected')
+io.on('connection', (socket) => {
+    socket.on('newMessage', (msg) => {
+        io.emit('newMessage', msg);
+        console.log('newChat : ' + msg);
+    })
+    socket.on('disconnect', (msg) => {
+        console.log('user disconnected');
+    })
 })
 const port = 3001
 server.listen(port, () => {
